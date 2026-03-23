b4fff6199dad7b1bf82eddeb0b186f00fafb2789
Sri <167113331+Sriwhocodes@users.noreply.github.com>
2026-03-23 13:57:37 +0530

    Delete src directory

--- src/components/ui/demo.tsx
+++
@@ -1,9 +0,0 @@
-import { SpecialText } from '@/components/ui/special-text';
-
-export default function Demo() {
-  return (
-    <div className="flex items-center justify-center min-h-[400px] p-8">
-      <SpecialText className="text-4xl font-bold">Make it beautiful</SpecialText>
-    </div>
-  );
-}
